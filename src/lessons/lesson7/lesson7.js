e5e6aa52292cfb7d78f525864f8bcb079cd9043a
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-06-22 18:36:26 +0300

    7 lesson - prototypes - done!

--- src/lessons/lesson7/lesson7.js
+++ src/lessons/lesson7/lesson7.js
@@ -10,3 +10,34 @@ Function.prototype.defer= function (delay){
 }
 
 //f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
+
+
+
+function Animal(name = 'Animal') {
+    this.name = name;
+    // this.__proto__ = animal;
+    this.walk = function (){
+        console.log(`${this.name} is walking `)
+    }
+    this.eat = function (){
+        console.log(`${this.name} is eating `)
+    }
+    this.sleep = function (){
+        console.log(`${this.name} is sleeping `)
+    }
+}
+const animal = new Animal();
+// animal.eat()
+
+function Human(name = 'Human') {
+    this.name = name;
+    this.__proto__ = Animal;
+    this.speak = function (){
+        console.log(`${this.name} is speaking `)
+    }
+    this.think = function (){
+        console.log(`${this.name} is thinking `)
+    }
+}
+const human = new Human()
+human.speak()
